ae3bd0bb484190dd057cd9a4a9cc66498ecfd705
shreejan <shreejan.sp@gmail.com>
2019-03-01 20:25:34 +0545

    added new folder

---
+++ final/src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+      apiKey: "AIzaSyA1N07ZJKik86coDW3tj9LMptNBnOLPeuk",
+      authDomain: "potent-thought-230216.firebaseapp.com",
+      databaseURL: "https://potent-thought-230216.firebaseio.com",
+      projectId: "potent-thought-230216",
+      storageBucket: "potent-thought-230216.appspot.com",
+      messagingSenderId: "52700293175"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
